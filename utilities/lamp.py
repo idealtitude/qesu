e9c3e60be59d4fff28bf9520423ae316ddfdb6b5
idealtitude <idealtitude@gmail.com>
2025-05-06 11:44:08 +0200

    Corrected and improved ./utilities/lamp.py

--- utilities/lamp.py
+++ utilities/lamp.py
@@ -1,35 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+@brief This small Python utility allows you to simply
+and easily manage httpd and mariadb services
+"""
+
 import sys
 import os
-
 import subprocess
 import argparse
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Services manager for the  Lamp server', prog='lamp')
+def parse_arguments() -> argparse.Namespace:
+    """Parsing command line arguments"""
+    parser = argparse.ArgumentParser(
+        description='Services manager for the  Lamp server', prog='lamp'
+    )
 
     parser.add_argument('cmd',
-                        nargs=1,
-                        choices=['start', 'restart', 'reload', 'stop', 'status'],
-                        help='Commande to apply for the service(s) chosen')
+        nargs=1,
+        choices=['start', 'restart', 'reload', 'stop', 'status'],
+        help='Commande to apply for the service(s) chosen'
+    )
     parser.add_argument('services',
-                        nargs='+',
-                        choices=['web', 'sql'],
-                        help='Type of services: web (httpd), sql (mariadb)')
+        nargs='+',
+        choices=['web', 'sql'],
+        help='Type of services: web (httpd), sql (mariadb)'
+    )
 
     parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1')
 
     return parser.parse_args()
 
-def exec_cmd(cmd):
-    res = subprocess.run(cmd)
-    print(res)
 
-def main():
-    args = parse_arguments()
+def exec_cmd(cmd) -> None:
+    """Actually executing the systemctl commands"""
+    try:
+        result: subprocess.CompletedProcess = subprocess.run(cmd)
+        print(result.stdout.strip())
+    except (subprocess.CalledProcessError, Exception) as ex:
+        print(f"\033[91mError\033[0m\nCommand execution failed:\n{ex}")
+
+
+def main() -> int:
+    """Main function, Entry Point"""
+    args: argparse.Namespace = parse_arguments()
 
     if os.geteuid() == 1000:
         print("You must be root to run this script!")
@@ -58,6 +74,7 @@ def main():
         exec_cmd(cmd)
 
     return 0
+
 
 if __name__ == '__main__':
     sys.exit(main())
